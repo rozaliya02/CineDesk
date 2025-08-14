20aa4cc28bea6d85c67e76aeda3a2639d71d018f
rozaliya02 <103183688+rozaliya02@users.noreply.github.com>
2025-08-14 16:17:25 +0300

    add search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Search = () => {
-  return <div className="text-white text-3xl">Search</div>;
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="../search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search thousands of movies, TV shows and more"
+          value={searchTerm}
+          onChange={event => setSearchTerm(event.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
